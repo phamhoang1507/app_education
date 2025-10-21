cfa407771d45efaa9fdf5d8539036b4e8a72cb47
hoangpd <hoangpd@miagi-so.net>
2025-10-21 08:18:16 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:education_app/screens/welcome_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WelcomePage(),
+    );
+  }
+}
